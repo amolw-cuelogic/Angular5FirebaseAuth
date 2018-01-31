78828409bfdef28837e4fba6b653ea6736aa6305
amolw-cuelogic <amol.wabale@cuelogic.com>
2018-01-31 19:55:29 +0530

    added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from './providers/auth.service';
+import { AngularFireAuth, AngularFireAuthProvider, AngularFireAuthModule } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [AngularFireAuth, AuthService]
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    photoUrl: any;
+    constructor(public authService: AuthService) {
+
+    }
+
+    login() {
+        this.authService.loginWithGoogle().then((data) => {
+            console.log(data.user.photoURL);
+            this.photoUrl = data.user.photoURL;
+        })
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AuthService } from './providers/auth.service';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyDMQ55IMy9XFdhggn0w9ru4x5ECXcnY9qo",
+    authDomain: "cuelogicresourcemanagement.firebaseapp.com",
+    databaseURL: "https://cuelogicresourcemanagement.firebaseio.com",
+    projectId: "cuelogicresourcemanagement",
+    storageBucket: "cuelogicresourcemanagement.appspot.com",
+    messagingSenderId: "563336911403"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+    providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
